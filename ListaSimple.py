a389098b4a50ebad6c7ce4e8507da947499bfef6
HeinzGomez <199819617@ingenieria.usac.edu.gt>
2019-07-28 22:48:42 -0600

    update lista
    tarea completada

--- ListaSimple.py
+++ ListaSimple.py
@@ -1,7 +1,7 @@
 from Nodo import Nodo
 class ListaSimple():
 
-	def __init(self):
+	def __init__(self):
 		self.inicio = None
 		self.fin = None
 
@@ -23,16 +23,17 @@ class ListaSimple():
 			self.inicio = xNodo
 			self.fin = xNodo
 		else:
-			self.fin.siguiente = xNodo.siguiente
+			self.fin.siguiente = xNodo
 			self.fin = xNodo
 
 	def eliminar_inicio(self):
 		if self.listavacia() == False:
-			xNono = self.inicio.siguiente
+			print("Eliminando el dato " + self.inicio.dato)
+			xNodo = self.inicio.siguiente
 			self.inicio = xNodo			
 		else:
-			print("La Lista esta vacia")
-
+			print("La Lista esta vacia \n\n")
+		print("\n\n")
 
 	def eliminar_fin(self):
 		if self.listavacia() == False:
@@ -40,33 +41,66 @@ class ListaSimple():
 			while xNodo.siguiente != self.fin:
 				xNodo = xNodo.siguiente
 			xNodo.siguiente = None
-			self.fin + xNodo
+			self.fin = xNodo
 		else:
 			print("La Lista esta vacia")
 
+	def eliminar_uno(self,lDato):
+		if self.listavacia() == True:
+			print("La Lista esta vacia \n\n")
+		else:
+			print("Dato a eliminar: " + lDato)			
+			if self.inicio.dato == lDato:
+				self.eliminar_inicio();
+			elif self.fin.dato == lDato:
+				self.eliminar_fin()
+			else:
+				xNodo = self.inicio	
+				while xNodo != self.fin:
+					axNodo = xNodo
+					if xNodo.siguiente != None:						
+						if xNodo.siguiente.dato == lDato:
+							axNodo = xNodo.siguiente
+							xNodo.siguiente = axNodo.siguiente
+							axNodo.siguiente = None
+						else:
+							xNodo = xNodo.siguiente
+					
 	def vaciar(self):
 		if self.listavacia() == True:
-			print("La Lista esta vacia")
+			print("La Lista esta vacia \n\n")
 		else:
+			#xNodo = self.inicio
 			while self.listavacia() == False:
 				self.eliminar_inicio()
 
-
 	def listar(self):
 		xNodo = self.inicio
-		while xNodo != None:
-			print (xNodo(dato))
-			xNodo = xNodo.siguiente 
+		#x = 0
+		if self.listavacia() == True:
+			print("La Lista esta vacia \n\n")
+		else:	
+			print("Vamos a obtener los datos:\n")
+			while xNodo != None:			
+				print (xNodo.dato)
+				xNodo = xNodo.siguiente 
+		print("\n\n")
 
 	def modificar(self,xDato, xNuevoDato):
 		if self.listavacia() ==True:
-			print("La Lista esta vacia")
+			print("La Lista esta vacia\n\n")
 		else:
-			xNodo = self.inicio
-			while xNodo.siguiente != None:
-				if xNodo.dato == xDato:
-					xNodo.dato = xNuevoDato
-
-				xNodo = xNodo.siguiente
+			if self.inicio.dato == xDato:
+				self.inicio.dato = xNuevoDato 
+			elif self.fin.dato == xDato:
+				self.fin.dato = xNuevoDato
+			else:
+				xNodo = self.inicio
+				print("va a entrar al while")
+				while xNodo.siguiente != None:
+					print("entro en el while")
+					if xNodo.dato == xDato:
+						xNodo.dato = xNuevoDato
+					xNodo = xNodo.siguiente
 
 
--- Principal.py
+++ Principal.py
@@ -1,27 +1,189 @@
+import os
+import time
 from ListaSimple import ListaSimple
 
+lst = ListaSimple()
+
+if os.name == "posix":
+   var = "clear"        
+elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+   var = "cls"
+
 def AgregarInicio():
-	print ("Agregando en la lista al inicio")
+	os.system(var) 
+	opSbMenu = 0
+	print ("""
+*****************************************************************
+***************  Agregando al inicio en la lista  ***************
+*****************************************************************
+""")
+	pDato = input("Escriba el Dato a Ingresar: ")
+	lst.insertar_inicio(pDato)
+
+	while opSbMenu != 99:
+		resp = str (input("Desea Agregar Otro Dato: s/n? "))
+		if resp == "s":
+			opSbMenu = 0
+			pDato = input("Escriba el Dato a Ingresar: ")
+			lst.insertar_inicio(pDato)
+		elif resp == "S":
+			opSbMenu = 0
+			pDato = input("Escriba el Dato a Ingresar: ")
+			lst.insertar_inicio(pDato)
+		else:
+			opSbMenu = 99
+
 def AgregarFin():
-	print("Agregando al final de la lista")
+	os.system(var) 
+	opSbMenu = 0
+	print ("""
+*****************************************************************
+***************  Agregando al final  en la lista  ***************
+*****************************************************************
+""")
+	pDato = input("Escriba el Dato a Ingresar: ")
+	lst.insertar_fin(pDato)
+
+	while opSbMenu != 99:
+		resp = str (input("Desea Agregar Otro Dato: s/n? "))
+		if resp == "s":
+			opSbMenu = 0
+			pDato = input("Escriba el Dato a Ingresar: ")
+			lst.insertar_fin(pDato)
+		elif resp == "S":
+			opSbMenu = 0
+			pDato = input("Escriba el Dato a Ingresar: ")
+			lst.insertar_fin(pDato)
+		else:
+			opSbMenu = 99
+
 def Recorrer():
-	print("Reccorriendo la lista")
+	os.system(var) 
+	print ("""
+*****************************************************************
+***************       Recorrido de la lista       ***************
+*****************************************************************
+""")
+	opSbMenu = 0
+	if lst.listavacia() == True:
+		print("La lista esta vacia \n")
+		input("Presione una tecla para continuar")
+	else:	
+		lst.listar()	
+		input("Presione una tecla para continuar \n")
+
 def ModificarDato():
-	print("Modificar un dato existente")
+	os.system(var) 
+	print ("""
+*****************************************************************
+***************  Modificar un datos de la lista   ***************
+*****************************************************************
+""")
+	lDato =""
+	bDato = ""
+	if lst.listavacia() == True:
+		print("La lista esta vacia \n")
+		input("Presione una tecla para continuar: ")
+	else:
+		lst.listar()
+		opSbMenu = 0
+		lDato = str(input("Ingrese el dato a Modificar: "))
+		bDato = str(input("Ingrese el nuevo Dato: "))
+		lst.modificar(lDato,bDato)
+		while opSbMenu != 99:
+			lst.listar()
+			resp = str (input("Desea Modificar Otro Dato: s/n? "))
+			if resp == "s":
+				opSbMenu = 0
+				lDato = str(input("Ingrese el dato a Modificar: "))
+				bDato = str(input("Ingrese el nuevo Dato: "))
+				lst.modificar(lDato,bDato)
+			elif resp == "S":
+				opSbMenu = 0
+				lDato = str(input("Ingrese el dato a Modificar: "))
+				bDato = str(input("Ingrese el nuevo Dato: "))
+				lst.modificar(lDato,bDato)
+			else:
+				opSbMenu = 99
+
+
 def EliminarInicio():
-	print("Eliminiar el inicio de la lista")
+	os.system(var) 
+	print ("""
+*****************************************************************
+***************  Eliminar al primero de la lista  ***************
+*****************************************************************
+""")
+	lst.listavacia()
+	input("Presione una tecla para continura\n")
+	lst.eliminar_inicio()
+	lst.listar()
+	input("Presione una tecla para continura\n")
+
+
 def EliminarFin():
-	print("Eliminar el final de la lista")
-def VaciarLista():
-	print("Vanciando la lista")
-def Salir():
-	print("Saliendo del sistema...")
-	exit()
+	os.system(var) 
+	print ("""
+*****************************************************************
+***************  Eliminar al ultimo de la lista   ***************
+*****************************************************************
+""")
+	lst.listar()
+	input("Presione una tecla para continura\n")
+	lst.eliminar_fin()
+	lst.listar()
+	input("Presione una tecla para continura\n")
+
+
+def Eliminar_uno():
+	os.system(var)	
+	print ("""
+*****************************************************************
+***************   Eliminar un dato de la lista.   ***************
+*****************************************************************
+""")
+	lDato =""
+	if lst.listavacia() == True:
+		print("La lista esta vacia \n")
+		input("Presione una tecla para continuar: ")
+	else:
+		lst.listar()
+		opSbMenu = 0
+		lDato = str(input("Ingrese el dato a eliminar: "))
+		lst.eliminar_uno(lDato)
+		while opSbMenu != 99:
+			lst.listar()
+			resp = str (input("Desea Elimiar Otro Dato: s/n? "))
+			if resp == "s":
+				opSbMenu = 0
+				lDAto = str(input("Ingrese el dato a eliminar: "))
+				lst.eliminar_uno(lDato)
+			elif resp == "S":
+				opSbMenu = 0
+				lDAto = str(input("Ingrese el dato a eliminar: "))
+				lst.eliminar_uno(lDato)
+			else:
+				opSbMenu = 99
+		#lst.listar()	
+
+def VaciarLista():	
+	os.system(var) 
+	print ("""
+*****************************************************************
+***************   Eliminando datos de la lista.   ***************
+*****************************************************************
+""")	
+	lst.vaciar()
+	#time.sleep(2)
+	input("Presione una tecla para continura\n")
 
 def Menu():
+	os.system(var) 
 	opMenu = 0
 	
 	while opMenu != 99:
+		os.system(var)
+		
 		print ("*****************************************************")
 		print ("***   Menu Principal Lista Simplemente Enlazada   ***")
 		print ("******************************************************")
@@ -31,11 +193,13 @@ def Menu():
 		print ("3 -> Listar")
 		print ("4 -> Modificar Registro")
 		print ("5 -> Eliminar Inicio")
-		print ("6 -> Eliminar Fin")			
-		print ("7 -> Vaciar  Lista")
-		print ("99 -> Salir")
+		print ("6 -> Eliminar Fin")	
+		print ("7 -> Eliminar Un Dato")		
+		print ("8 -> Vaciar Lista")
+		print ("99 -> Salir \n")
 
-		opMenu = input("Seleccione Opcion: ")
+		opMenu = int (input("Seleccione Opcion: "))
+		#print(opMenu)
 
 		if opMenu == 1: #Agregar un registro al inicio de la lista
 			AgregarInicio()
@@ -46,14 +210,14 @@ def Menu():
 		elif opMenu == 4:#Modifica un dato dentro de la lista
 			ModificarDato()
 		elif opMenu == 5:#Elimina el inicio de la lista
-			EliminaInicio()
+			EliminarInicio()
 		elif opMenu == 6:#Elimina el final de la lista
-			EliminaFin()
-		elif opMenu == 7:#vacia la lista
+			EliminarFin()
+		elif opMenu == 7:
+			Eliminar_uno()	
+		elif opMenu == 8:#vacia la lista
 			VaciarLista()
-		elif opMenu == 99:#Sale del sistema
-			Salir()
+		#elif opMenu == 99:#Sale del sistema
+		#	Salir()
 
 Menu()
-
-
